e593ddd9ff33e380af2c589eb39b5458f9651f0a
Gokul <gokulshyam567@gmail.com>
2024-06-01 17:33:46 -0400

    fix: replace oracle jdk with openjdk

--- install_script.sh
+++ install_script.sh
@@ -2,13 +2,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 #Install Java
-sudo add-apt-repository ppa:linuxuprising/java -y
-
-sudo apt-get update
-
-echo oracle-java17-installer shared/accepted-oracle-license-v1-3 select true | sudo debconf-set-selections
-sudo apt-get install oracle-java17-installer -y
-sudo apt-get install oracle-java17-set-default -y
+sudo apt install openjdk-17-jdk openjdk-17-jre -y
 
 # Install Jenkins
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
